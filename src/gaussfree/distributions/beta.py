1cbf3ad5ac22daa39c655e2c0ee2f248b955b7f4
kkovlakas <kkovla@gmail.com>
2024-08-30 12:22:07 +0200

    Small changes to the beta distribution tools.

--- src/gaussfree/distributions/beta.py
+++ src/gaussfree/distributions/beta.py
@@ -32,33 +32,42 @@ def get_unimodal_beta(mode, variance, verbose=False):
         The SciPy beta distribution object.
 
     """
-    def verbose_print(*args, **kwargs):
+    def vprint(*args, **kwargs):
+        """Verbose print function."""
         if verbose:
             print(*args, **kwargs)
 
-    if variance >= UNIMODAL_BETA_MAX_VAR:
-        raise ValueError(f"Not unimodal for variance = {variance:.6g} >= "
-                         f"{UNIMODAL_BETA_MAX_VAR:.6g}.")
+    vprint(f"Input properties: mode={mode:.3g}, var={variance:.3g}")
 
-    verbose_print(f"Input pararms : mode={mode:.3g}, variance={variance:.3g}")
+    if not 0 < mode < 1:
+        raise ValueError(f"Mode must be in (0, 1), got {mode:.6g}.")
+    if variance <= 0:
+        raise ValueError(f"Variance must be positive, got {variance:.6g}.")
+    if variance >= UNIMODAL_BETA_MAX_VAR:
+        raise ValueError(f"Not unimodal for variance greater than or equal to "
+                         f"{UNIMODAL_BETA_MAX_VAR:.6g}, got {variance:.6g}.")
 
     def func(x):
-        u, w = x
-        a = 1 + np.exp(u)
-        b = 1 + np.exp(w)
+        a, b = 1 + np.exp(x)
         new_mode = (a - 1.0) / (a + b - 2.0)
         new_variance = a * b / ((a + b)**2.0 * (a + b + 1.0))
         return [new_mode - mode, new_variance - variance]
 
     sol = root(func, [0, 0], method="lm")
-    assert sol.success
+    if not sol.success:
+        raise RuntimeError("Failed to find the beta parameters. "
+                           "Root finder returned:\n" + str(sol))
+
     a, b = 1.0 + np.exp(sol.x)
-    verbose_print(f"Beta params   : a={a:.6g}, b={b:.6g}")
+    vprint(f"Beta parameters : a={a:.6g}, b={b:.6g}")
+
     rec_mode = (a - 1.0) / (a + b - 2.0)
     rec_variance = a * b / ((a + b) ** 2.0 * (a + b + 1.0))
-    verbose_print(
-        f"Recovered par.: mode={rec_mode:.6g}, variance={rec_variance:.6g}")
-    assert np.isclose(mode, rec_mode) and np.isclose(variance, rec_variance)
+    vprint(f"Recovered stats : mode={rec_mode:.6g}, var={rec_variance:.6g}")
+    if not np.isclose(mode, rec_mode) and np.isclose(variance, rec_variance):
+        raise AssertionError("Failed to recover the input mode and variance."
+                             "Run with verbose=True to see the details.")
+
 
     dist = st.beta(a, b)
     if verbose:
@@ -66,5 +75,8 @@ def get_unimodal_beta(mode, variance, verbose=False):
         plt.figure()
         plt.plot(x, dist.pdf(x), "k-")
         plt.xlim(0, 1)
+        plt.ylim(ymin=0)
+        plt.xlabel("x")
+        plt.ylabel("Probability density")
         plt.show()
     return dist
